c9c165340b1043ebbe4ff11d4cb41ac2bb86f264
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-10-16 18:13:42 +0100

    commit add header component to page

--- App/app/page.tsx
+++ App/app/page.tsx
@@ -1,8 +1,10 @@
 import TopSection from "@/components/dashboard/topSection/topSection";
+import Header from "@/components/otherComponents/Header/Header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <TopSection />
     </>
   )
