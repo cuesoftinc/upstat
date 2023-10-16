741319aaf86478e5f0b6d09fe434b8e82762273a
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-10-16 10:12:42 +0100

    Update types for topSection

--- App/components/dashboard/topSection/topSection.tsx
+++ App/components/dashboard/topSection/topSection.tsx
@@ -9,8 +9,60 @@ import {
 } from "./topSection.styles"
 
 
-const TopSection = () => {
+const downChart = "streamline:money-graph-arrow-decrease-down-stats-graph-descend-right-arrow"
+const upChart = "streamline:money-graph-arrow-increase-ascend-growth-up-arrow-stats-graph-right-grow"
 
+const statsData: {
+    id: number,
+    name: string,
+    number: string,
+    percent: string,
+    chart: string,
+    color: string,
+}[] = [
+    {
+        id: 0,
+        name: "Web Views",
+        number: "658k",
+        percent: "+3.08",
+        chart: upChart,
+        color: "#00E09E"
+    },
+    {
+        id: 1,
+        name: "Unique Visitors",
+        number: "246k",
+        percent: "-8.36",
+        chart: downChart,
+        color: "#FF3D5A"
+    },
+    {
+        id: 2,
+        name: "New Users",
+        number: "1352",
+        percent: "+2.65",
+        chart: upChart,
+        color: "#00E09E"
+    },
+    {
+        id: 3,
+        name: "Page VIew",
+        number: "438",
+        percent: "-0.37",
+        chart: downChart,
+        color: "#FF3D5A"
+    },
+    {
+        id: 0,
+        name: "Active Users",
+        number: "294k",
+        percent: "-5.12",
+        chart: downChart,
+        color: "#FF3D5A"
+    },
+]
+
+const TopSection = () => {
     const statsTsx = statsData.map(el => {
         return (
             <StatsContainer key={el.id}>
@@ -39,51 +91,5 @@ const TopSection = () => {
         </TopSectionContainer>
     )
 }
-
-const down = "streamline:money-graph-arrow-decrease-down-stats-graph-descend-right-arrow"
-const up = "streamline:money-graph-arrow-increase-ascend-growth-up-arrow-stats-graph-right-grow"
-
-const statsData = [
-    {
-        id: 0,
-        name: "Web Views",
-        number: "658k",
-        percent: "+3.08",
-        chart: up,
-        color: "#00E09E"
-    },
-    {
-        id: 1,
-        name: "Unique Visitors",
-        number: "246k",
-        percent: "-8.36",
-        chart: down,
-        color: "#FF3D5A"
-    },
-    {
-        id: 2,
-        name: "New Users",
-        number: "1352",
-        percent: "+2.65",
-        chart: up,
-        color: "#00E09E"
-    },
-    {
-        id: 3,
-        name: "Page VIew",
-        number: "438",
-        percent: "-0.37",
-        chart: down,
-        color: "#FF3D5A"
-    },
-    {
-        id: 0,
-        name: "Active Users",
-        number: "294k",
-        percent: "-5.12",
-        chart: down,
-        color: "#FF3D5A"
-    },
-]
 
 export default TopSection
