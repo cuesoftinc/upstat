5cd7353402e2da72d6318e5dd5d8c2f795a9bbd9
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-10-17 23:03:58 +0100

    move bottom section  styles to it's file

--- App/components/dashboard/bottomSection/bottomSection.tsx
+++ App/components/dashboard/bottomSection/bottomSection.tsx
@@ -1,9 +1,16 @@
 "use client"
 
-import { styled } from "styled-components"
 import BarChart from "../charts/BarChat"
 import DonoughtChart from "../charts/DonoughtChart"
 import { locationTrafficData } from "../data"
+import {
+    DonoughtSectionContainer,
+    DonoughtDetailsContainer,
+    DonoughtChartContainer,
+    BarChartContainer,
+    BottomContainer,
+    DetailItems,
+} from "./BottomSection.styles"
 
  
 const BottomSection = () => {
@@ -13,7 +20,7 @@ const BottomSection = () => {
             <DetailItems>
                 <div style={{background: el.color}}></div>
                 <p>{el.name}</p>
-                <p>{el.percent}%</p>
+                <p style={{justifySelf: "flex-end"}}>{el.percent}%</p>
             </DetailItems>
         )
     })
@@ -34,57 +41,5 @@ const BottomSection = () => {
         </BottomContainer>
     )
 }
-
-const BottomContainer = styled.section`
-    display: flex;
-    gap: 29px;
-    width: 100%;
-    background: #16151C;
-    padding: 21px 40px 43px 25px;
-    color: #fff
-`
-
-const DonoughtSectionContainer = styled.div`
-    width: 50%;
-    display: flex;
-    align-items: center;
-    background: #3c3c3c;
-    border-radius: 10px;
-    padding: 26px 41px 27px 29px;
-    gap: 40px;
-    height: 100%;
-`
-
-const DonoughtDetailsContainer = styled.div`
-    width: 50%;
-    display: flex; 
-    flex-direction: column;
-    gap: 19px;
-`
-
-const DetailItems = styled.div`
-    display: flex;
-    gap: 20px;
-
-    div {
-        width: 15px;
-        height: 15px;
-        border-radius: 50%;
-    }
-    
-`
-
-const BarChartContainer = styled.div`
-    width: 50%;
-    padding: 25px 36px 22px 21px;
-    min-height: 266px;
-    background: #3c3c3c;
-    border-radius: 10px;
-`
-
-const DonoughtChartContainer = styled.div`
-    width: 50%;
-    height: 100%;
-`
 
 export default BottomSection
