b80e8ae081f7e215e33176107eb4f5482a83bfc7
BrightOlawale <oolawalebright@gmail.com>
2023-10-19 13:00:28 +0100

    feat: gRPC User Services with Auth

--- CommonServer/main.go
+++ CommonServer/main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/CuesoftCloud/upstat/routes"
-	"github.com/gin-gonic/gin"
+	"github.com/CuesoftCloud/upstat/config"
+	pb "github.com/CuesoftCloud/upstat/proto"
+	"github.com/CuesoftCloud/upstat/services"
+	"github.com/CuesoftCloud/upstat/utils"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"sync"
 )
 
-// main is the entry point for the server
+var wg sync.WaitGroup
+
+//func unaryInterceptor(
+//	ctx context.Context,
+//	req interface{},
+//	info *grpc.UnaryServerInfo,
+//	handler grpc.UnaryHandler,
+//) (interface{}, error) {
+//	log.Println("--> unary interceptor: ", info.FullMethod)
+//	return handler(ctx, req)
+//}
+
 func main() {
-	server := gin.Default()
-	routes.SetupRoutes(server)
-	err := server.Run(":8080")
+	// Load environment variables
+	config.LoadEnv()
+
+	lis, err := net.Listen("tcp", "[::1]:8080")
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalln("App: Failed to start server:", err)
 	}
+	log.Println("Server listening on port 8080")
+
+	// Create gRPC server
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(utils.AuthenticateInterceptor),
+	)
+	service := &services.UserServiceServer{}
+
+	// Register the service with the server
+	pb.RegisterUserServiceServer(grpcServer, service)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done() // Mark this goroutine as done when it finishes
+		err := grpcServer.Serve(lis)
+		if err != nil {
+			log.Fatalln("gRPC: Failed to start server:", err)
+		}
+	}()
+
+	log.Println("gRPC server started")
+
+	// Wait for both servers to start before exiting
+	wg.Wait()
 }
