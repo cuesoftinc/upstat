e8cdc1201d2b9a9c672406b3e8e13b324deafe04
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-10-19 10:16:34 +0100

    rename files and resturcture folders

--- App/data/dashboardData.ts
+++ App/data/dashboardData.ts
@@ -1,3 +1,57 @@
+
+const downChart = "streamline:money-graph-arrow-decrease-down-stats-graph-descend-right-arrow"
+const upChart = "streamline:money-graph-arrow-increase-ascend-growth-up-arrow-stats-graph-right-grow"
+
+export const statsData: {
+    id: number,
+    name: string,
+    number: string,
+    percent: string,
+    chart: string,
+    color: string,
+}[] = [
+    {
+        id: 0,
+        name: "Web Views",
+        number: "658k",
+        percent: "+3.08",
+        chart: upChart,
+        color: "#00E09E"
+    },
+    {
+        id: 1,
+        name: "Unique Visitors",
+        number: "246k",
+        percent: "-8.36",
+        chart: downChart,
+        color: "#FF3D5A"
+    },
+    {
+        id: 2,
+        name: "New Users",
+        number: "1352",
+        percent: "+2.65",
+        chart: upChart,
+        color: "#00E09E"
+    },
+    {
+        id: 3,
+        name: "Page VIew",
+        number: "438",
+        percent: "-0.37",
+        chart: downChart,
+        color: "#FF3D5A"
+    },
+    {
+        id: 0,
+        name: "Active Users",
+        number: "294k",
+        percent: "-5.12",
+        chart: downChart,
+        color: "#FF3D5A"
+    },
+]
+
 export const totalUserData: {
   id: number,
   day: string,
--- App/data/trafficData.ts
+++ App/data/trafficData.ts
@@ -1,7 +1,7 @@
-import user1 from "../../assets/images/user1.png";
-import user2 from "../../assets/images/user2.png";
-import user3 from "../../assets/images/user3.png";
-import user4 from "../../assets/images/user4.png";
+import user1 from "@/assets/images/user1.png";
+import user2 from "@/assets/images/user2.png";
+import user3 from "@/assets/images/user3.png";
+import user4 from "@/assets/images/user4.png";
 
 export const userdata: {
   id: number;
