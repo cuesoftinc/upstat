2a215545de52ad6900aa1ef61d4527b23626848e
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-11-11 10:29:50 +0100

    move styles to seperate file

--- App/components/uptime/topSection/TopSection.tsx
+++ App/components/uptime/topSection/TopSection.tsx
@@ -1,6 +1,11 @@
-"use client"
-
-import { styled } from "styled-components"
+import {
+    TopsectionContainer,
+    OperationStat,
+    UpDownTime,
+    StatusBar,
+    DownTime,
+    Uptime,
+} from "./topSection.styles"
 
 type topsectionProp = {
     system : string
@@ -41,72 +46,5 @@ const Topsection = ({system}: topsectionProp) => {
     )
 
 }
-
-const TopsectionContainer = styled.section`
-    background: #16151C;
-    display: flex;
-    gap: 21px;
-    color: #fff;
-`
-
-const OperationStat = styled.div`
-    display: flex;
-    width: 80%;
-    background: #3C3C3C;
-    gap: 10px;
-    align-items: center;
-    padding: 42px 17px;
-    border-radius: 10px;
-    max-width: 881px;
-    
-
-    div {
-        width: 18px;
-        height: 18px;
-        background: rgba(0, 224, 158, 0.62);
-        border-radius: 50%;
-    }
-`
-
-const UpDownTime = styled.div`
-    display: flex;
-    gap: 19px;
-    flex-direction: column;
-    width: 20%;
-    padding: 24px 40px 29px 10px;
-    background: #3C3C3C;
-    border-radius: 10px;
-    max-width: width: 881px;
-`
-
-const Uptime = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 20px;
-
-    div {
-        display: flex;
-        gap: 3.7px;
-    }
-`
-
-const DownTime = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 20px;
-
-    div {
-        display: flex;
-        gap: 3.7px;
-    }
-`
-
-const StatusBar = styled.div`
-    min-width: 5.113px;
-    min-height: 24.104px;
-    border-radius: 3.652px;
-`
 
 export default Topsection
