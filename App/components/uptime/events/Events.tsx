2c724f761791e931e8f7d15f54752ca7540b01ae
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-11-16 15:23:39 +0100

    move event styles to seperate folder

--- App/components/uptime/events/Events.tsx
+++ App/components/uptime/events/Events.tsx
@@ -1,5 +1,9 @@
 import { Icon } from "@iconify/react"
-import { styled } from "styled-components"
+import {
+    EventsContainer,
+    EventsHeading,
+    EventDetail,
+} from "./Events.styles"
 
 const eventData: {
     event: string,
@@ -34,34 +38,5 @@ const Events = () => {
         </EventsContainer>
     )
 }
-
-const EventsContainer = styled.section`
-    width: 100%;
-    background: #3C3C3C;
-    border-radius: 10px;
-    padding: 15px 22px 25px 14px;
-    color: #fff;
-`
-
-const EventsHeading = styled.div`
-    display: flex;
-    padding-bottom: 17px;
-    font-size: 20px;
-    justify-content: space-between;
-`
-
-const EventDetail = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    padding: 15px 0 11px;
-    border-top: 0.5px solid rgba(255, 255, 255, 0.50);
-
-    span {
-        font-size: 14px;
-        color: rgba(255, 255, 255, 0.50);
-        font-weight: 500;
-    }
-`
 
 export default Events
