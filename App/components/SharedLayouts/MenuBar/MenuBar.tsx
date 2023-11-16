feeb2636f60fe1b072d4d3198b3d40c959a52c7e
bragg <58727101+Brag2gr8@users.noreply.github.com>
2023-11-16 15:21:16 +0100

    refactor menubar component

--- App/components/SharedLayouts/MenuBar/MenuBar.tsx
+++ App/components/SharedLayouts/MenuBar/MenuBar.tsx
@@ -12,6 +12,7 @@ import {
     MenuTitle,
     MenuItem
 } from "./MenuBar.styles"
+import { accountData, menudata } from "@/data/menuBarData"
 
 
 const MenuBar = () => {
@@ -67,75 +68,5 @@ const MenuBar = () => {
  )
 }
 
-const menudata: {
-    id: number,
-    icon: string,
-    name: string,
-    path: string,
-}[] = [
-    {
-        id: 0,
-        icon: "material-symbols:dashboard",
-        name: "Dashboard",
-        path:"/"
-    },
-    {
-        id: 1,
-        icon: "ph:traffic-sign-fill",
-        name: "Traffic",
-        path:"/traffic"
-    },
-    {
-        id: 2,
-        icon: "bxs:hourglass",
-        name: "Uptime",
-        path:"/uptime"
-    },
-
-    {
-        id: 3,
-        icon: "mingcute:time-fill",
-        name: "Page Load Time",
-        path:"/pageloadtime"
-    },
-    {
-        id: 4,
-        icon: "mdi:analytics",
-        name: "SEO Metrics",
-        path:"/seo"
-    },
-    {
-        id: 5,
-        icon: "fluent:arrow-bounce-16-filled",
-        name: "Bounce Rate",
-        path:"/bounce"
-    },
-    {
-        id: 6,
-        icon: "material-symbols:error",
-        name: "Error Rate",
-        path:"/error"
-    }
-]
-
-const accountData: {
-    id: number,
-    icon: string,
-    name: string,
-    path: string,
-}[] = [
-    {
-        id: 1,
-        icon: "icon-park-solid:setting",
-        name: "Settings",
-        path:"/settings"
-    },
-    {
-        id: 2,
-        icon: "mdi:help-box",
-        name: "Help Center",
-        path:"/help"
-    },
-]
 
 export default MenuBar
