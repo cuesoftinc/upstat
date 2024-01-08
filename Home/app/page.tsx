32b355be2ef22e834d77daa280501bc6650b4458
Darab-gif <darabcode@gmail.com>
2024-01-08 16:00:27 +0100

    implemented the hero section

--- Home/app/page.tsx
+++ Home/app/page.tsx
@@ -4,10 +4,12 @@ import styled from "styled-components";
 import Workflow from "../components/workflow/Workflow";
 import BusinessNeed from "@/components/businessNeed/BusinessNeed";
 import OpenSource from "@/components/openSource/openSource/OpenSource";
+import HeroSection from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <main>
+      <HeroSection />
       <CodingView />
       <Workflow />
       <BusinessNeed />
---
+++ Home/components/headerBar/HeaderBar.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  HeaderWrapper,
+  List,
+  ListContainer,
+  LoginWrapper,
+  LogoImage,
+} from "./Header.style";
+import logo from "../../assets/upstat_logo.png";
+import { Icon } from "@iconify/react";
+
+const HeaderBar = () => {
+  return (
+    <HeaderWrapper>
+      <LogoImage src={logo} alt="logo" />
+      <ListContainer>
+        <div className="github">
+          <span>
+            <Icon icon="uiw:github" />
+          </span>{" "}
+          <p>18,439</p>
+        </div>
+        <List>
+          <p>Product</p>
+          <span>
+            <Icon icon="ic:sharp-keyboard-arrow-down" />
+          </span>
+        </List>
+        <List>
+          <p>Solutions</p>
+          <span>
+            <Icon icon="ic:sharp-keyboard-arrow-down" />
+          </span>
+        </List>
+        <List>
+          <p>Resources</p>
+          <span>
+            <Icon icon="ic:sharp-keyboard-arrow-down" />
+          </span>
+        </List>
+        <List>
+          <p>Contact</p>
+          <span>
+            <Icon icon="ic:sharp-keyboard-arrow-down" />
+          </span>
+        </List>
+        <p>Docs</p>
+      </ListContainer>
+      <LoginWrapper>
+        <button className="login">Login</button>
+        <button className="signup">Sign up</button>
+      </LoginWrapper>
+    </HeaderWrapper>
+  );
+};
+
+export default HeaderBar;
