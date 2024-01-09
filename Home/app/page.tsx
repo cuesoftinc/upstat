4397f7dfcb4dbeca8f51286b4c3d57e42033f159
Darab-gif <darabcode@gmail.com>
2024-01-09 08:44:34 +0100

    implemented the tools and partner section

--- Home/app/page.tsx
+++ Home/app/page.tsx
@@ -5,11 +5,13 @@ import Workflow from "../components/workflow/Workflow";
 import BusinessNeed from "@/components/businessNeed/BusinessNeed";
 import OpenSource from "@/components/openSource/openSource/OpenSource";
 import HeroSection from "@/components/hero/Hero";
+import ToolSection from "@/components/tools/Tools";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <ToolSection />
       <CodingView />
       <Workflow />
       <BusinessNeed />
