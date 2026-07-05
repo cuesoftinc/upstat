beb3beaeb70cab9de8ae7022067d781611f3293a
Sinen Ernest <sinenernest43@gmail.com>
2026-07-05 10:29:53 +0100

    merge conflict resolved and sub-repo normalized

--- node_modules/styled-components/native/dist/dist/utils/isKeyframes.d.ts
+++
@@ -1,4 +0,0 @@
-import type KeyframesClass from '../models/Keyframes';
-declare const KEYFRAMES_SYMBOL: unique symbol;
-export default function isKeyframes(value: unknown): value is KeyframesClass;
-export { KEYFRAMES_SYMBOL };
